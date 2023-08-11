2e17bd8e71c135c3a50a5968a2d06bd71b212509
Jihan Chowdhury <j.chowdhury@asthait.com>
2023-08-11 11:39:58 +0600

    remote changed

--- src/server.ts
+++ src/server.ts
@@ -15,12 +15,10 @@ let server: Server;
 async function bootstrap() {
   try {
     await mongoose.connect(config.databaseURL as string);
-    // console.log(`🛢   Database is connected successfully`);
-    console.log(`🛢   Database is connected successfully`);
+    console.log(`Database is connected successfully`);
 
     server = app.listen(config.port, () => {
-      // console.log(`Application  listening on port ${config.port}`);
-      console.log(`Application  listening on port ${config.port}`);
+      console.log(`App listening on port ${config.port}`);
     });
   } catch (err) {
     console.log('Failed to connect database', err);
